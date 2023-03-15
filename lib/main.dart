228ff066892a8f1d49a3ab67ce780fc358f6f921
AllanRanier <allanranier18@gmail.com>
2023-03-15 19:27:59 -0300

    pathUrl

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:site/app/app_widgets.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(const AppWidgets());
 }
