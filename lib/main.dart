9c3cd6d45c93eb67469d93d94b352564fd10874d
AllanRanier <allanranier18@gmail.com>
2023-03-15 19:35:38 -0300

    linkTree

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'package:site/app/app_widgets.dart';
-import 'package:url_strategy/url_strategy.dart';
-
-void main() {
-  setPathUrlStrategy();
-  runApp(const AppWidgets());
-}
